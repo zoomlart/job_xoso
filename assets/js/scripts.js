053cdd04888589468df745ecea379b5296e93925
Nguyen Quoc <zoomlart@gmail.com>
2022-08-24 03:14:03 +0700

    update livestream

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44,3 +44,14 @@ setInterval(function(){
     currentTime = getDateTime();
     document.getElementById("digital-clock").innerHTML = currentTime;
 }, 1000);
+$('.input-number-increment').click(function() {
+    var $input = $(this).parents('.input-number-group').find('.input-number');
+    var val = parseInt($input.val(), 10);
+    $input.val(val + 1);
+  });
+  
+  $('.input-number-decrement').click(function() {
+    var $input = $(this).parents('.input-number-group').find('.input-number');
+    var val = parseInt($input.val(), 10);
+    $input.val(val - 1);
+  });
