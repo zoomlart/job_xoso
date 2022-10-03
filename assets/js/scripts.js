61a332e9f22963188465db53da6f4cee731066e6
Nguyen Quoc <zoomlart@gmail.com>
2022-10-03 15:26:52 +0700

    update layout fullwidth

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -58,3 +58,17 @@ $('.input-number-increment').click(function() {
   $(document).ready(function(){
     $('[data-bs-toggle="tooltip"]').tooltip(); 
 });
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "bi-eye-slash-fill" );
+            $('#show_hide_password i').removeClass( "bi-eye-fill" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "bi-eye-slash-fill" );
+            $('#show_hide_password i').addClass( "bi-eye-fill" );
+        }
+    });
+});
